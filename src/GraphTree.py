c84d980c9679ed340c1afa24978c0550af60504f
AngeloDamante <angelo.damante16@gmail.com>
2023-02-16 23:30:12 +0100

    wip: working on make_graph function

--- src/GraphTree.py
+++ src/GraphTree.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
+from src.graph_maker import create_node, create_topic
 from typing import Tuple
 
 SIZE = (720, 1280, 3)
 BG_FRAME = np.ones(SIZE, np.uint8) * 245
-SPACE = (100, 100)  # space between nodes
-
+SPACE = (50, 50)  # space between nodes
+INIT_NODES = (50, 50)
+INIT_TOPICS = (150, 50)
 
 
 
@@ -14,4 +16,21 @@ def make_graph(nodes: list, topics: list, incidence_matrix: list, background: np
     if len(nodes) != len(incidence_matrix): return False, np.zeros(background.shape)
     if len(topics) != len(incidence_matrix[0]): return False, np.zeros(background.shape)
 
-    # cv2.imwrite("image.png", img)
+    # create nodes
+    origin_node = list(INIT_NODES)
+    num_line = 1
+    img = BG_FRAME.copy()
+    for node in nodes:
+        img, tl, br = create_node(node, origin_node, background=img)
+        origin_node = [br[0] + SPACE[0], tl[1]]
+
+        if origin_node[0] > SIZE[0]:
+            num_line += 1
+            origin_node = [INIT_NODES[0], INIT_NODES[1] * num_line]
+
+    # create topics TODO
+
+    return True, img
+
+
+
