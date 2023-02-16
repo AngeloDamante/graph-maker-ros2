b1e7b4fe74d8580899307b8865d109d6e85d8b56
AngeloDamante <angelo.damante16@gmail.com>
2023-02-16 20:50:08 +0100

    test: add test for create_topic

--- test/test_maker.py
+++ test/test_maker.py
@@ -1,8 +1,7 @@
 import unittest
 from src.ENodeType import NodeType
-from src.graph_maker import create_node
+from src.graph_maker import create_node, create_topic
 import cv2
-
 
 topics = ['/t1', '/t2', 't3', '/t4', 't5']
 nodes = ['/camera/img_sender', '/n2', '/n3']
@@ -10,16 +9,16 @@ incidence_matrix = [[NodeType.PUB, NodeType.NULL, NodeType.SUB, NodeType.NULL, N
                     [NodeType.NULL, NodeType.PUB, NodeType.SUB, NodeType.NULL, NodeType.SUB],
                     [NodeType.PUB, NodeType.PUB, NodeType.PUB, NodeType.SUB, NodeType.SUB]]
 
+
 class TestMaker(unittest.TestCase):
 
-    def test_make_node(self):
-        img = create_node(nodes[0], [50,50])
-        cv2.imwrite("image.png", img)
+    def test_create_node(self):
+        img = create_node(nodes[0], [50, 50])
+        cv2.imwrite("image_node.png", img)
 
-    def test_make_graph_failure(self):
-        # empty case
-        # Not correct format
-        pass
+    def test_create_topic(self):
+        img = create_topic(topics[0], [50, 50])
+        cv2.imwrite("image_topic.png", img)
 
 
 class TestGraph(unittest.TestCase):
