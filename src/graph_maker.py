3a488ff1a4b30efc50665e3fba5a1caf82da58bb
AngeloDamante <angelo.damante16@gmail.com>
2023-02-16 21:49:58 +0100

    feat: fix policies, add feat to compute bb

--- src/graph_maker.py
+++ src/graph_maker.py
@@ -3,43 +3,56 @@
 """
 import cv2
 import numpy as np
+from typing import Tuple
 
 SIZE = (720, 1280, 3)
 BG_FRAME = np.ones(SIZE, np.uint8) * 245
 BORDER = [10, 10]
 
 
-def create_node(name: str, origin: list, background: np.ndarray = None) -> np.ndarray:
+def create_node(name: str, origin: list, background: np.ndarray = None) -> Tuple[np.ndarray, tuple, tuple]:
     """Create Graphic Node for input name.
 
     :param name:
     :param origin:
     :param background:
-    :return image(np.ndarray): image with ellipse
+    :return:
+        image(np.ndarray): image with ellipse
+        top_left(tuple)
+        bottom_right(tuple)
     """
-    if background is None: background = BG_FRAME.copy()
-    img = background
-
-    # extract dimension
-    (lbl_w, lbl_h), _ = cv2.getTextSize(name, fontScale=0.8, fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, thickness=1)
-
-    # text
-    ap = [origin[0], origin[1] + lbl_h]
-    img = cv2.putText(img, name, ap, fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.8, color=(0, 0, 100))
-
-    # ellipse
-    center = [origin[0] + lbl_w // 2, origin[1] + lbl_h // 2]
-    img = cv2.ellipse(img, center, (lbl_w // 2 + BORDER[0], lbl_h // 2 + BORDER[1]), angle=0, startAngle=0, endAngle=360, color=(0, 0, 0), thickness=1)
-    return img
+    img, top_left, bottom_right = compute_bb(name, origin, background)
+    center = [(top_left[0] + bottom_right[0]) // 2, (top_left[1] + bottom_right[1]) // 2]
+    axis_x, axis_y = (bottom_right[0] - top_left[0]) // 2, (bottom_right[1] - top_left[1]) // 2
+    img = cv2.ellipse(img, center, (axis_x, axis_y), angle=0, startAngle=0, endAngle=360, color=(0, 0, 0), thickness=1)
+    return img, tuple(top_left), tuple(bottom_right)
 
 
-def create_topic(name: str, origin: list, background: np.ndarray = None) -> np.ndarray:
-    """Create graphic topic for input name
+def create_topic(name: str, origin: list, background: np.ndarray = None) -> Tuple[np.ndarray, tuple, tuple]:
+    """Create graphic Topic for input name
 
     :param name:
     :param origin:
     :param background:
-    :return image(np.ndarray): image with rectangle
+    :return:
+        image(np.ndarray): image with rectangle
+        top_left(tuple)
+        bottom_right(tuple)
+    """
+    img, top_left, bottom_right = compute_bb(name, origin, background)
+    img = cv2.rectangle(img, top_left, bottom_right, color=(0, 0, 0), thickness=2)
+    return img, tuple(top_left), tuple(bottom_right)
+
+def compute_bb(name: str, origin: list, background: np.ndarray = None) -> Tuple[np.ndarray, tuple, tuple]:
+    """Compute Bounding Box coords and image with text.
+
+    :param name:
+    :param origin:
+    :param background:
+    :return:
+        image with applied text
+        top_left(tuple)
+        bottom_right(tuple)
     """
     if background is None: background = BG_FRAME.copy()
     img = background
@@ -51,9 +64,8 @@ def create_topic(name: str, origin: list, background: np.ndarray = None) -> np.n
     ap = [origin[0], origin[1] + lbl_h]
     img = cv2.putText(img, name, ap, fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.8, color=(0, 0, 100))
 
-    # rectangle
+    # bb
     top_left = [origin[0] - BORDER[0], origin[1] - BORDER[1]]
     bottom_right = [origin[0] + lbl_w + BORDER[0], origin[0] + lbl_h + BORDER[1]]
-    img = cv2.rectangle(img, top_left, bottom_right, color=(0, 0, 0), thickness=2)
-    return img
+    return img, tuple(top_left), tuple(bottom_right)
 
