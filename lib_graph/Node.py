e76f37bf0306cc93afd9a77f60805b54517eb3aa
AngeloDamante <angelo.damante16@gmail.com>
2023-02-17 18:49:50 +0100

    reformat

--- lib_graph/Node.py
+++
@@ -1,9 +0,0 @@
-"""
-    Derived class Node from GraphicElement.
-"""
-from lib_graph.GraphicElement import GraphicElement
-
-
-class Node(GraphicElement):
-    def __init__(self, name: str, top_left: tuple, bottom_right: tuple):
-        super().__init__(name, top_left, bottom_right)
--- lib_graph/Topic.py
+++
@@ -1,9 +0,0 @@
-"""
-    Derived class Topic from GraphicElement.
-"""
-from lib_graph.GraphicElement import GraphicElement
-
-
-class Topic(GraphicElement):
-    def __init__(self, name: str, top_left: tuple, bottom_right: tuple):
-        super().__init__(name, top_left, bottom_right)
