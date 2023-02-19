f01da940071a900b4688566d96f9436326c93e22
AngeloDamante <angelo.damante16@gmail.com>
2023-02-19 11:46:11 +0100

    test: add test for node and topic into drawer
    the test produce image with node and node

--- test/test_drawer.py
+++ test/test_drawer.py
@@ -4,18 +4,21 @@ from src.draw_elements import draw_node, draw_topic, compute_inner_bb, draw_conn
 from src.Drawer import Drawer
 import cv2
 
+NODE = '/camera/img_sender'
+TOPIC = '/camera/topic'
+
 
 class TestDrawLib(unittest.TestCase):
     def test_create_node(self):
-        img, _, _ = draw_node('/camera/img_sender', (50, 50))
+        img, _, _ = draw_node(NODE, (50, 50))
         cv2.imwrite("image_node.png", img)
 
     def test_create_topic(self):
-        img, _, _ = draw_topic('/camera/topic', (50, 50))
+        img, _, _ = draw_topic(TOPIC, (50, 50))
         cv2.imwrite("image_topic.png", img)
 
     def test_compute_bb(self):
-        img, tl, br = compute_inner_bb('/camera/img_sender', (50, 50))
+        img, tl, br = compute_inner_bb(NODE, (50, 50))
         cv2.imwrite("image_bb.png", img)
 
     def test_draw_connection(self):
@@ -27,14 +30,37 @@ class TestDrawLib(unittest.TestCase):
         cv2.imwrite("image_null.png", img)
 
 
-class TestDrawer(unittest.TestCase):
+class TestDrawerClass(unittest.TestCase):
     def test_make_drawer(self):
-        o_drawer = Drawer(origin = (50,50), size = (1280,720,3))
+        o_drawer = Drawer(origin=(50, 50), size=(1280, 720, 3))
         self.assertEqual(o_drawer.is_valid(), True)
 
     def test_make_drawer_failure(self):
-        o_drawer = Drawer(origin = (50,50), size = (1280,72))
+        o_drawer = Drawer(origin=(50, 50), size=(1280, 72))
         self.assertEqual(o_drawer.is_valid(), False)
+        o_drawer = Drawer(origin=(50, 50, 1), size=(1280, 72))
+        self.assertEqual(o_drawer.is_valid(), False)
+        o_drawer = Drawer(origin=(50, 50), size=(1280, 72, 3), color_bg=(100, 100))
+        self.assertEqual(o_drawer.is_valid(), False)
+
+    def test_reset_drawer(self):
+        o_drawer = Drawer(origin=(50, 50), size=(640, 720, 3))
+        cv2.imwrite("blank_image.png", o_drawer.get_img())
+        o_drawer.add_node(NODE)
+        cv2.imwrite("node_image.png", o_drawer.get_img())
+        o_drawer.reset_drawer()
+        cv2.imwrite("reset_image.png", o_drawer.get_img())
+
+    def test_node_topic(self):
+        o_drawer = Drawer(origin=(50, 50), size=(640, 720, 3))
+        o_drawer.add_node(NODE)
+        o_drawer.add_topic(TOPIC)
+        cv2.imwrite("image.png", o_drawer.get_img())
+
+    def test_reset_drawer_failure(self):
+        o_drawer = Drawer(origin=(50, 50, 1), size=(1280, 72))
+        cv2.imwrite("blank_image.png", o_drawer.get_img())
+        o_drawer.reset_drawer()
 
 
 if __name__ == '__main__':
