a3f4054b612eabeb1014d0fe54f394408dce33ed
AngeloDamante <angelo.damante16@gmail.com>
2023-02-23 00:41:22 +0100

    feat: add compute_bb method and fix returns type

--- src/draw_elements.py
+++ src/draw_elements.py
@@ -9,6 +9,7 @@ import numpy as np
 from typing import Tuple
 from src.ENodeType import NodeType
 
+# default image
 SIZE = (720, 1280, 3)
 WHITE = (254, 254, 254)
 IMG_BG = np.ones(SIZE, np.uint8)
@@ -16,70 +17,57 @@ IMG_BG[:, :, :] = WHITE
 BORDER = [10, 10]
 
 
-def compute_inner_bb(name: str, origin: tuple, img_bg: np.ndarray = None) -> Tuple[np.ndarray, tuple, tuple]:
-    """Compute Bounding Box coords and image with text.
+def compute_bb(name: str, origin: tuple, border: tuple = (10, 10)) -> Tuple[tuple, tuple]:
+    """Compute bounding box coords with border
+
+    For inner bounding box, select border = (0,0)
 
     :param name:
     :param origin:
-    :param img_bg:
-    :return:
-        image with applied text
-        top_left(tuple)
-        bottom_right(tuple)
+    :param border:
+    :return: top_left, bottom_right
     """
-    if img_bg is None: img_bg = IMG_BG.copy()
-
-    # extract dimension
     (lbl_w, lbl_h), _ = cv2.getTextSize(name, fontScale=0.8, fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, thickness=1)
-
-    # text
-    ap = [origin[0], origin[1] + lbl_h]
-    img = cv2.putText(img_bg, name, ap, fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.8, color=(0, 0, 100))
-
-    # bb
-    top_left = [origin[0], origin[1]]
-    bottom_right = [origin[0] + lbl_w, origin[1] + lbl_h]
-    return img, tuple(top_left), tuple(bottom_right)
+    top_left = (origin[0] - border[0], origin[1] - border[1])
+    bottom_right = (origin[0] + lbl_w + border[0], origin[1] + lbl_h + border[1])
+    return top_left, bottom_right
 
 
-def draw_node(name: str, origin: tuple, img_bg: np.ndarray = None, border=None) -> Tuple[np.ndarray, tuple, tuple]:
-    """Create Graphic Node for input name.
+def draw_node(name: str, origin: tuple, border: tuple = (10, 10), img_bg: np.ndarray = None) -> np.ndarray:
+    """Create Graphic Node for input name
 
     :param name:
     :param origin:
     :param img_bg:
     :param border:
-    :return:
-        image(np.ndarray): image with ellipse
-        top_left(tuple)
-        bottom_right(tuple)
+    :return: image(np.ndarray): image with ellipse
     """
-    if border is None: border = BORDER
-    img, top_left, bottom_right = compute_inner_bb(name, origin, img_bg)
-    center = [(top_left[0] + bottom_right[0]) // 2, (top_left[1] + bottom_right[1]) // 2]
+    if img_bg is None: img_bg = IMG_BG.copy()
+    top_left, bottom_right = compute_bb(name, origin, (0, 0))
+    c = [(top_left[0] + bottom_right[0]) // 2, (top_left[1] + bottom_right[1]) // 2]
     axis_x, axis_y = (bottom_right[0] - top_left[0]) // 2 + border[0], (bottom_right[1] - top_left[1]) // 2 + border[1]
-    img = cv2.ellipse(img, center, (axis_x, axis_y), angle=0, startAngle=0, endAngle=360, color=(0, 0, 0), thickness=1)
-    return img, tuple(top_left), tuple(bottom_right)
+    ap = [origin[0], bottom_right[1]]
+    img = cv2.putText(img_bg, name, ap, fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.8, color=(0, 0, 100))
+    img = cv2.ellipse(img, c, (axis_x, axis_y), angle=0, startAngle=0, endAngle=360, color=(0, 0, 0), thickness=1)
+    return img
 
 
-def draw_topic(name: str, origin: tuple, img_bg: np.ndarray = None, border=None) -> Tuple[np.ndarray, tuple, tuple]:
+def draw_topic(name: str, origin: tuple, border: tuple = (10, 10), img_bg: np.ndarray = None, ) -> np.ndarray:
     """Create graphic Topic for input name
 
     :param name:
     :param origin:
     :param img_bg:
     :param border:
-    :return:
-        image(np.ndarray): image with rectangle
-        top_left(tuple)
-        bottom_right(tuple)
+    :return: image(np.ndarray): image with rectangle
     """
-    if border is None: border = BORDER
-    img, top_left, bottom_right = compute_inner_bb(name, origin, img_bg)
-    top_left = (top_left[0] - border[0], top_left[1] - border[1])
-    bottom_right = (bottom_right[0] + border[0], bottom_right[1] + border[1])
+    if img_bg is None: img_bg = IMG_BG.copy()
+    top_left, bottom_right = compute_bb(name, origin, (0, 0))
+    ap = [origin[0], bottom_right[1]]
+    img = cv2.putText(img_bg, name, ap, fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.8, color=(0, 0, 100))
+    top_left, bottom_right = compute_bb(name, origin, border)
     img = cv2.rectangle(img, top_left, bottom_right, color=(0, 0, 0), thickness=2)
-    return img, tuple(top_left), tuple(bottom_right)
+    return img
 
 
 def draw_connection(node: tuple, topic: tuple, img_bg: np.ndarray = None, action: NodeType = NodeType.NULL) -> np.ndarray:
