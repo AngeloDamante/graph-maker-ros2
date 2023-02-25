1e9509fad6808d9c442d9dc481b18239697f58c9
AngeloDamante <angelo.damante16@gmail.com>
2023-02-25 03:19:34 +0100

    test: for make_graph in GraphMaker

--- test/test_maker.py
+++ test/test_maker.py
@@ -1,4 +1,5 @@
 import unittest
+import cv2
 from src.ENodeType import NodeType
 from GraphMaker import GraphMaker
 
@@ -13,13 +14,20 @@ bad_incidence_matrix = [[NodeType.PUB, 0, NodeType.SUB, NodeType.NULL, NodeType.
                         [NodeType.PUB, NodeType.PUB, NodeType.PUB, NodeType.SUB, NodeType.SUB]]
 
 class TestMaker(unittest.TestCase):
-    def test_make_graph(self):
+    def test_graph_maker(self):
         o_gm = GraphMaker(nodes, topics, incidence_matrix)
         self.assertEqual(o_gm.is_valid(), True)
 
-    def test_make_graph_failure(self):
+    def test_graph_maker_failure(self):
         o_gm = GraphMaker(nodes, topics, bad_incidence_matrix)
         self.assertEqual(o_gm.is_valid(), False)
+
+    def test_make_graph(self):
+        o_gm = GraphMaker(nodes, topics, incidence_matrix)
+        b_flag = o_gm.make_graph()
+        self.assertEqual(b_flag, True)
+        cv2.imwrite("image.png", o_gm.get_graph())
+
 
 
 if __name__ == '__main__':
